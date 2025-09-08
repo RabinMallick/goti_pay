5d6d228d381efc8e0634613c47cad6228a325b7b
Apurbo Kumar Anup <apurboka@gmail.com>
2025-09-08 17:40:58 +0600

    Update NetBanking.tsx

--- src/components/UI/payement-gateway/NetBanking.tsx
+++ src/components/UI/payement-gateway/NetBanking.tsx
@@ -1,84 +1,69 @@
 'use client';
 
-import Image from 'next/image';
 import React, { FC, useState } from 'react';
 import { motion } from 'framer-motion';
-import { CiCircleCheck } from 'react-icons/ci';
 import MerchantNoticeCard from '@/components/common/card/MerchantNoticeCard';
 import ProceedingCard from '@/components/common/card/ProceedingCard';
+import LogoCard from './LogoCard';
 
-const logos = [
-    '/bkash.svg',
-    '/rocket.svg',
-    '/upay.svg',
-    '/stpay.svg',
-    '/pathao.svg',
-    '/mcash.svg',
-    '/cellfin.svg',
-    '/pocket.svg',
-    '/nagad.svg',
+const logos: string[] = [
+  '/bkash.svg',
+  '/rocket.svg',
+  '/upay.svg',
+  '/stpay.svg',
+  '/pathao.svg',
+  '/mcash.svg',
+  '/cellfin.svg',
+  '/pocket.svg',
+  '/nagad.svg',
 ];
 
 const NetBanking: FC = () => {
-    const [selected, setSelected] = useState<number | null>(null);
+  const [selected, setSelected] = useState<number | null>(null);
 
-    return (
-        <form>
-            <motion.div
-                key="cardForm"
-                initial={{ opacity: 0, y: 15 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -15 }}
-                transition={{ duration: 0.25 }}
-            >
-                <p className="text-[12px] mb-1 text-gray-600">
-                    Select your desired Payment method
-                </p>
+  const handleSelect = (index: number) => {
+    setSelected(index);
+  };
 
-                <div className="grid grid-cols-3 gap-3 mt-3">
-                    {logos.map((logo, index: number) => (
-                        <div
-                            key={index}
-                            onClick={() => setSelected(index)}
-                            className={`flex justify-center items-center h-16 relative rounded-md cursor-pointer
-      ${selected === index
-                                    ? 'border-[var(--primary)] ring-1 ring-[var(--primary)]'
-                                    : ''
-                                }`}
-                        >
-                            <div className=" w-full h-full relative">
-                                <Image
-                                    src={logo}
-                                    alt={`logo-${index}`}
-                                    fill
-                                    className="object-contain p-2"
-                                />
-                            </div>
-                            {selected === index && (
-                                <div className="absolute right-1 top-1 text-sm text-green-800">
-                                    <CiCircleCheck />
-                                </div>
-                            )}
-                        </div>
-                    ))}
-                </div>
+  return (
+    <form>
+      <motion.div
+        key="cardForm"
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -15 }}
+        transition={{ duration: 0.25 }}
+      >
+        <p className="text-[12px] mb-1 text-gray-600">
+          Select your desired Payment method
+        </p>
 
-               <ProceedingCard/>
+        <div className="grid grid-cols-3 gap-3 mt-3">
+          {logos.map((logo, index) => (
+            <LogoCard
+              key={index}
+              logo={logo}
+              selected={selected === index}
+              onClick={() => handleSelect(index)}
+            />
+          ))}
+        </div>
 
+        <ProceedingCard />
 
-                <MerchantNoticeCard />
+        <MerchantNoticeCard />
 
-                <motion.button
-                    type="submit"
-                    className="w-full bg-[var(--primary)] text-white py-1 text-base md:text-[13px] rounded-md transition my-3 mt-6 cursor-pointer"
-                    whileHover={{ scale: 1.02 }}
-                    whileTap={{ scale: 0.96 }}
-                >
-                    Pay 1,000.00 BDT
-                </motion.button>
-            </motion.div>
-        </form>
-    );
+        <motion.button
+          type="submit"
+          className="w-full bg-[var(--primary)] text-white py-1 text-base md:text-[13px] rounded-md transition my-3 mt-6 cursor-pointer"
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.96 }}
+        >
+          Pay 1,000.00 BDT
+        </motion.button>
+      </motion.div>
+    </form>
+  );
 };
 
 export default NetBanking;
