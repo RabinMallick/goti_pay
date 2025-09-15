82eb67487fcf607203e9ea8dd8c12ccf65b9de58
Apurbo Kumar Anup <apurboka@gmail.com>
2025-09-15 15:54:51 +0600

    Update SimplifyingPayments.tsx

--- src/components/common/SimplifyingPayments.tsx
+++ src/components/common/SimplifyingPayments.tsx
@@ -55,10 +55,10 @@ const SimplifyingPayments: FC = () => {
     return (
        <div className="py-16 ">
          <div className="max-w-7xl mx-auto  ">
-            <h2 className="text-3xl md:text-4xl font-bold text-center mb-10">
+            <h2 className="text-xl sm:text-2xl lg:text-4xl font-bold text-center mb-10">
                 Simplifying payments for every <span className="text-blue-600">industry</span>
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8 px-4">
                 {industries.map((industry, idx) => (<motion.div
                     className="relative w-full h-64 rounded-xl shadow-xl cursor-pointer perspective"
                     whileHover={{ rotateY: 15, rotateX: 5, scale: 1.05 }}
